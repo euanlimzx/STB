031640fc05133c6464069712b29196e6301221aa
euanlimzx <euanlimzx@gmail.com>
2024-04-30 16:28:21 +0800

    baseline ts

--- ts.js
+++ ts.js
@@ -60,35 +60,33 @@ async function timeSeriesCSVBuilder(resourceId) {
 
     // We assume there is no case where 1 column has all `na` values, which is impossible to represent in the format STB provides
     let allColumns = [];
+    // console.log(JSON.stringify(res.data.Data.row))
     for (const { columns } of res.data.Data.row) {
       if (columns.length > allColumns.length) {
         allColumns = columns.map(({ key }) => key);
       }
     }
-    res.data.Data.row.forEach((row, idx) => {
-      const { columns, rowText, seriesNo, uoM: uomValue } = row;
-      const prefix = (seriesNo.match(/\./g) || []).reduce(
-        (a) => `${a}    `,
-        ""
-      );
-      uoM ??= uomValue;
+    res.data.Data.row.forEach(({ columns, rowText, seriesNo, uoM: uomValue }, idx) => {
+      const prefix = (seriesNo.match(/\./g) || []).reduce((a) => `${a}    `, '')
+
+      uoM ??= uomValue
 
       for (const { key, value } of columns) {
         // If row does not exist, create row with initial column
         rows[idx] ??= {
           [DATA_SERIES_MACHINE_NAME]: prefix + rowText, // Title for column header
-        };
+        }
 
         // If we do not have the machine name for this column, generate and set the machine name
         if (!nameCache.has(key)) {
-          nameCache.set(key, normalizeHumanName(key));
-          typeCache.set(key, "NUMERIC");
+          nameCache.set(key, normalizeHumanName(key))
+          typeCache.set(key, "NUMERIC")
         }
 
-        rows[idx][nameCache.get(key)] = value; // Set the value for the column
+        rows[idx][nameCache.get(key)] = value // Set the value for the column
 
         if (typeCache.get(key) !== "TEXT" && !isNumeric(value)) {
-          typeCache.set(key, "TEXT");
+          typeCache.set(key, "TEXT")
         }
       }
 
@@ -96,36 +94,25 @@ async function timeSeriesCSVBuilder(resourceId) {
         // If we have not set all columns for this row, set the remaining columns to `na`
         for (const column of allColumns) {
           if (!rows[idx][nameCache.get(column)]) {
-            typeCache.set(column, "TEXT");
+            typeCache.set(column, "TEXT")
           }
         }
       }
-    });
+    })
 
-    offset++;
-  }
-  console.log(rows.map((row)=>row["DataSeries"]))
-  for (const idx in rows) {
-    if (rows[idx] && Object.keys(rows[idx]).length < nameCache.size) {
-      // If we have not set all columns for this row, set the remaining columns to `na`
-      for (const column of nameCache.values()) {
-        if (!rows[idx][nameCache.get(column)]) {
-          typeCache.set(column, "TEXT");
-        }
-      }
-    }
+    offset++
   }
 
   const columnOrderSOT = [
     DATA_SERIES_MACHINE_NAME,
     ...Array.from(nameCache.values()).slice(1).reverse(),
-  ];
+  ]
 
   const parser = new AsyncParser({
     // Singstat likes to show latest first
     fields: columnOrderSOT,
-    defaultValue: "na", // Match STB CSV generator
-  });
+    defaultValue: 'na', // Match STB CSV generator
+  })
 
   return {
     uoM,
@@ -133,26 +120,9 @@ async function timeSeriesCSVBuilder(resourceId) {
     machineToHumanNames: new Map(Array.from(nameCache, (e) => [e[1], e[0]])),
     humanNameToType: typeCache,
     rendered: await parser.parse(rows).promise(),
-    columnOrderSOT: columnOrderSOT,
-  };
+    columnOrderSOT
+  }
 }
 
 const csv = await timeSeriesCSVBuilder("M550002");
-let columnMetadata = csv.columnOrderSOT.map((machineReadableName) => {
-  const humanReadableName =
-    csv.machineToHumanNames.get(machineReadableName) ?? "x";
-  return {
-    humanReadableName,
-    machineReadableName,
-    dataType:
-      csv.humanNameToType.get(humanReadableName) ?? "TEXT",
-    unitOfMeasure: csv.uoM,
-  };
-});
-
-//columnMetadata = columnMetadata.sort((a, b) => b.machineReadableName.localeCompare(a.machineReadableName))
-//need to build a checker to see when dataseries is returned - isit at the beginning or the end?? fucksake
-//const isDataSeriesFirst = columnMetadata[0].machineReadableName == "DataSeries"
-//columnMetadata = isDataSeriesFirst? [columnMetadata[0],...columnMetadata.slice(1).reverse()] : [...columnMetadata.slice(-1),...columnMetadata.slice(0,-1).reverse()]
-
 createCSVFile(csv.rendered, "output.csv");
